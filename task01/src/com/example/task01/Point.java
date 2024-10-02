6ee7e97a1f941ab22d310b2ffe808e08e72aed19
sdybybhon <artem563732@mail.ru>
2024-10-03 00:34:38 +0500

    Уфимцев Артём. Задание 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,11 +1,27 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
     int x;
     int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip() {
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
+
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
