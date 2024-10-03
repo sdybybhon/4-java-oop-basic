ac26320e626e3dcbc0918d38194f01524dd074c9
sdybybhon <artem563732@mail.ru>
2024-10-04 00:21:09 +0500

    Уфимцев Артём. Задание 05

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,18 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        Point p3 = new Point(5, 6);
 
+        PolygonalLine polygonalLine = new PolygonalLine();
+        polygonalLine.addPoint(p1);
+        polygonalLine.addPoint(p2);
+        polygonalLine.addPoint(p3);
+
+        // примерно 5.66
+
+
+        System.out.println("Длина ломаной линии: " + polygonalLine.getLength());
     }
 }
